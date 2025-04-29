9854027e5a8ca4fda414b694c97f614252c1f385
Tishenski <vladtish5@stud.op.edu.ua>
2025-04-29 22:48:22 +0300

    Initial commit

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template, request, redirect, url_for, session, flash
+from flask import Flask, render_template, request, redirect, url_for, session, flash
 import sqlite3
 import os
 
@@ -26,6 +27,7 @@ def init_db():
 @app.route('/')
 def index():
     return redirect(url_for('login'))
+
 
 @app.route('/register', methods=['GET', 'POST'])
 def register():
